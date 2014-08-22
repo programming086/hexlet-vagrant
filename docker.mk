f4f3a9b9733fcd7a834eb81cb15e378ed8560f6e
Kirill Mokevnin <mokevnin@gmail.com>
2014-08-22 13:37:23 +0400

    update docker.mk

--- docker.mk
+++ docker.mk
@@ -1,23 +1,29 @@
 NEW_T := $(shell cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-CS = "$(shell docker ps -a -q)"
+CS = $(shell docker ps -a -q)
+
+IMAGE_ID_FILE = "/var/tmp/docker_image_id"
+IMAGE_ID = $(shell cat $(IMAGE_ID_FILE))
+
+CONTAINER_ID_FILE = "/var/tmp/docker_container_id"
+CONTAINER_ID = $(shell cat $(CONTAINER_ID_FILE))
 
 build:
-	export IMAGE_ID=$(NEW_T) && docker build -t $(NEW_T) .
+	docker build -t $(NEW_T) . && echo $(NEW_T) > $(IMAGE_ID_FILE)
 
 bash:
-	@ docker run -it $(shell cat tmp/imid) /bin/bash
+	@ docker run -it $(IMAGE_ID) /bin/bash
 
-run: stop
-	@ docker run -p 8080:8080 -dt -v $(CURDIR)/:/root/exercise $(IMAGE_ID)
+run: remove
+	@ docker run -p 8080:8080 -dt -v $(CURDIR)/:/root/exercise $(IMAGE_ID) > $(CONTAINER_ID_FILE) && echo "RUN"
 
 stop:
-	@ docker kill $(CS)
+	@ docker kill $(CS) > /dev/null 2>&1; echo "STOP"
 
 remove: stop
-	docker rm $(CS)
+	@ docker rm $(CS) > /dev/null 2>&1; echo "REMOVE"
 
 # CHECK RUNNING
 test:
-	@ sudo docker-enter $(shell cat tmp/cid) /bin/bash -c 'cd /root/exercise && ./run'
+	@ sudo docker-enter $(CONTAINER_ID) /bin/bash -c 'cd /root/exercise && ./run'
 
-.PHONY: test build bash run stop t mkdir
+.PHONY: test build bash run stop
